436be0102579ab2599e21cbe327f553a3785e631
Deutscher <81298350+Deutscher775@users.noreply.github.com>
2023-03-10 21:31:42 +0100

    config refering to alpha 0.1.0
    This config refers to alpha 0.1.0. Configs that are not marked with (soon) are needed.

--- conf.py
+++ conf.py
@@ -1,38 +1,55 @@
 import socket
 from urllib import parse
 import colorama
+import platform
 
-# the following code gets the ip of the server automatically
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    HOST_IP = s.getsockname()[0]
-except:
-    print(colorama.Fore.CYAN + "----\nYour server doesn't seem to be connected to the internet or blocks "
-                                 f"ips.\nRunning {colorama.Fore.LIGHTBLUE_EX}localhost.\n{colorama.Fore.CYAN}----" + colorama.Style.RESET_ALL)
-    HOST_IP = "localhost"
-
-# Please enter your os the panel is runnig on, just type "windows" or "linux"
-
-OS = "windows"
+os_get = platform.system().lower()
+if os_get == "windows":
+    OS = "windows"
+elif os_get == "linux":
+    OS = "linux"
+else:
+    print(colorama.Fore.RED + "Your operating system couldn't be detected or is not supported yet" + colorama.Style.RESET_ALL)
 
 # webserver config
 
 DEBUGGING = True
 PORT = 80
-SESSION_SECRET_KEY = "Some_Secret_K3y"
+SESSION_SECRET_KEY = "S0me_K3y"
 
 # OAuth config
 
-OAUTH_CLIENT_SECRET = ""
+DOMAIN_NAME = ""
+DOMAIN_PORT = 80
+
+if DOMAIN_NAME is str:
+    if DOMAIN_PORT is int:
+        AFTER_OAUTH_REDIRECT_URL = f"http://{DOMAIN_NAME}:{DOMAIN_PORT}/oauth/callback"  # if you change this, make sure to change it in the discord developer portal too
+        HOST_IP = DOMAIN_NAME
+    else:
+        AFTER_OAUTH_REDIRECT_URL = f"http://{DOMAIN_PORT}:{PORT}/oauth/callback"  # if you change this, make sure to change it in the discord developer portal too
+
+else:
+    # the following code gets the ip of the server automatically
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        HOST_IP = s.getsockname()[0]
+    except:
+        print(colorama.Fore.CYAN + "----\nYour server doesn't seem to be connected to the internet or blocks "
+                                   f"ips.\nRunning {colorama.Fore.LIGHTBLUE_EX}localhost.\n{colorama.Fore.CYAN}----" + colorama.Style.RESET_ALL)
+        HOST_IP = "localhost"
+    AFTER_OAUTH_REDIRECT_URL = f"http://{HOST_IP}:{PORT}/oauth/callback"  # if you change this, make sure to change it in the discord developer portal too
+
+
+OAUTH_CLIENT_SECRET = "
 OAUTH_TOKEN = ""
-OAUTH_CLIENT_ID = ""
-AFTER_OAUTH_REDIRECT_URL = f"http://{HOST_IP}:{PORT}/oauth/callback" # if you change this, make sure to change it in the discord developer portal too
+OAUTH_CLIENT_ID = 
 OAUTH_URL = f"https://discord.com/api/oauth2/authorize?client_id={OAUTH_CLIENT_ID}&redirect_uri={parse.quote(AFTER_OAUTH_REDIRECT_URL)}&response_type=code&scope=identify%20guilds"
 
 # Discord bot config
 
-DISCORD_BOT_TOKEN = ""
+DISCORD_BOT_TOKEN = "OTkxNjM3NDgzMDA1Njg5ODY2.G3wHMZ.a8k2CV8L6mxE5Mj-WttViuR46ITNtWCDC1sfBM"
 
 # Discord server config
 
@@ -41,10 +58,10 @@ DISCORD_SERVER_ID =
 # Discord allowed IDs config
 
 DISCORD_ACCESS_ROLE_ID = 
-DISCORD_PANEL_ADMINISTRATOR_ID = 
+DISCORD_PANEL_ADMINISTRATOR_ID =
 DISCORD_PANEL_FULL_ACCESS_USER_IDS = []
 
-# notification config
+# notification config (soon)
 
 NOTIFICATION_ON_DENIED_ACCESS = False
 NOTIFICATION_CHANNEL_ID = 
