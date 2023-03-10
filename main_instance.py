f58cebfcba586f16a7a8576c511777a76cd06418
Deutscher <81298350+Deutscher775@users.noreply.github.com>
2023-03-10 21:20:01 +0100

    release alpha 0.1.0

--- main_instance.py
+++ main_instance.py
@@ -1,4 +1,6 @@
 import os
+import platform
+import signal
 import sys
 import time
 import traceback
@@ -12,7 +14,7 @@ import psutil
 import pathlib
 import subprocess
 
-dash = flask.Flask("CeleryPanel", template_folder="./webpanel", static_folder="./webpanel")
+dash = flask.Flask("CeleryPanel", template_folder=f"{pathlib.Path(__file__).parent.resolve()}/webpanel", static_folder=f"{pathlib.Path(__file__).parent.resolve()}/webpanel")
 
 try:
     oauth_client = zenora.APIClient(token=conf.OAUTH_TOKEN, client_secret=conf.OAUTH_CLIENT_SECRET)
@@ -98,6 +100,10 @@ def bot_action(action):
 @dash.route("/", methods=["POST", "GET"])
 def route_index():
     try:
+        open(f"{pathlib.Path(__file__).parent.resolve()}/data/setup-start.json", "r")
+    except FileNotFoundError:
+        return flask.redirect("/setup")
+    try:
         if flask.session["logout"] is True:
             flask.session["logout"] = False
             return flask.redirect("/login")
@@ -141,7 +147,7 @@ def admin_panel():
 @dash.route("/administrator/log/login")
 def show_login_log():
     if "oauth_access_token" in flask.session:
-        f = open("log/login_log.txt", "r").read()
+        f = open(f"{pathlib.Path(__file__).parent.resolve()}/log/login_log.txt", "r").read()
         oauth_bearer_client = zenora.APIClient(flask.session["oauth_access_token"], bearer=True)
         user_id = oauth_bearer_client.users.get_current_user().id
         if user_id in conf.DISCORD_PANEL_FULL_ACCESS_USER_IDS:
@@ -193,12 +199,12 @@ async def login_callback():
         flask.session["login_site_notification"] = "An error occurred"
         return flask.redirect("/login")
     elif check_if_access(user.id):
-        open("./log/login_log.txt", "a").write(f"[{datetime.datetime.now()}] {user.id} | {user.username} | Granted\n")
+        open(f"{pathlib.Path(__file__).parent.resolve()}/log/login_log.txt", "a").write(f"[{datetime.datetime.now()}] {user.id} | {user.username} | Granted\n")
         flask.session["logout"] = False
         flask.session["login_site_notification"] = ""
         return flask.redirect("/")
     else:
-        open("./log/login_log.txt", "a").write(f"[{datetime.datetime.now()}] {user.id} | {user.username} | Refused\n")
+        open(f"{pathlib.Path(__file__).parent.resolve()}/log/login_log.txt", "a").write(f"[{datetime.datetime.now()}] {user.id} | {user.username} | Refused\n")
         flask.session["oauth_access_token"] = None
         flask.session["logout"] = False
         flask.session["login_site_notification"] = "You are not allowed to access this panel."
@@ -215,7 +221,9 @@ async def server_stats_iframe():
     cores = f"Core count: {psutil.cpu_count(logical=False)}"
     cpu_usage = f"CPU usage: {psutil.cpu_percent()}%"
     ram_usage = f"RAM usage: {psutil.virtual_memory().percent}%"
-    return flask.render_template("server_stats_iframe.html", cpu_cores=cores, cpu_use=cpu_usage, ram_use=ram_usage)
+    operating_system = f"Operating system: {platform.system()}"
+    return flask.render_template("server_stats_iframe.html", cpu_cores=cores,
+                                 cpu_use=cpu_usage, ram_use=ram_usage, os=operating_system)
 
 
 @dash.route("/administrator/log/login/clear", methods=["POST", "GET"])
@@ -236,9 +244,24 @@ async def bot_home():
     try:
         bot_total_server = get_bot_stats("total server")
         bot_user_name = get_bot_stats("bot user name")
+        def check_run(pid):
+            try:
+                os.kill(pid, 0)
+            except OSError:
+                return False
+            else:
+                return True
+
+        f = open(f"{pathlib.Path(__file__).parent.resolve()}/data/discord-bot-process.json", "r")
+        f_json = json.load(f)
+        pid = f_json["process-id"]
+        if check_run(pid) is True:
+            is_running = "Running"
+        else:
+            is_running = "Not running"
         return flask.render_template("bot.html", bot_total_servers=bot_total_server,
-                                     bot_user_name=bot_user_name,
-                                     action_log_response=flask.session["bot_action_response"])
+                                     bot_user_name=bot_user_name, is_running=is_running,
+                                     action_response=flask.session["bot_action_response"])
     except KeyError:
         flask.session["bot_action_response"] = ""
         return flask.redirect("/bot")
@@ -246,14 +269,23 @@ async def bot_home():
 
 @dash.route("/assets/logo", methods=["GET"])
 async def get_logo():
-    return flask.send_file("./webpanel/assets/CeleryPanel.png")
+    return flask.send_file(f"{pathlib.Path(__file__).parent.resolve()}/webpanel/assets/CeleryPanel.png")
 
 
 @dash.route("/bot/shutdown", methods=["POST", "GET"])
 async def bot_shutdown():
+    f = open(f"{pathlib.Path(__file__).parent.resolve()}/data/discord-bot-process.json", "r")
+    f_json = json.load(f)
+    pid = f_json["process-id"]
+    os.kill(pid, signal.SIGTERM)
     bot_action("shutdown")
     time.sleep(1)
     return flask.redirect("/bot")
+
+
+@dash.route("/log/server", methods=["POST", "GET"])
+async def server_logs():
+    return flask.render_template("/log/server_log.html", s_logs=sys.stdout)
 
 
 @dash.route("/bot/start", methods=["POST", "GET"])
@@ -268,29 +300,63 @@ async def bot_start():
     json.dump(communicator, communicator_file)
     communicator_file.truncate()
     communicator_file.close()
+    start_command_file = open(f"{pathlib.Path(__file__).parent.resolve()}/data/setup-start.json")
+    start_command_json = json.load(start_command_file)
     if conf.OS == "windows":
         subprocess.Popen(f"py {pathlib.Path(__file__).parent.resolve()}/discord_worker.py")
+        if start_command_json["start-command-set"] is True:
+            try:
+                process = subprocess.Popen(start_command_json["start-command-command"])
+                try:
+                    f = open(f"{pathlib.Path(__file__).parent.resolve()}/data/discord-bot-process.json", "x")
+                    data = {"process-id": process.pid}
+                    json.dump(data, f)
+                except FileExistsError:
+                    f = open(f"{pathlib.Path(__file__).parent.resolve()}/data/discord-bot-process.json", "w")
+                    f.truncate(0)
+                    data = {"process-id": process.pid}
+                    json.dump(data, f)
+            except Exception as e:
+                flask.session["bot_action_response"] = str(e)
+        else:
+            flask.session["bot_action_response"] = "No discord bot start set."
         time.sleep(3)
         return flask.redirect("/bot")
     elif conf.OS == "linux":
-        subprocess.Popen(f"python3 {pathlib.Path(__file__).parent.resolve()}/discord_worker.py")
+        subprocess.Popen(["python3", f"{pathlib.Path(__file__).parent.resolve()}/discord_worker.py"])
+        if start_command_json["start-command-set"] is True:
+            try:
+                process = subprocess.Popen(["python3", f"{start_command_json['start-command-command']}"])
+                try:
+                    f = open(f"{pathlib.Path(__file__).parent.resolve()}/data/discord-bot-process.json", "x")
+                    data = {"process-id": process.pid}
+                    json.dump(data, f)
+                except FileExistsError:
+                    f = open(f"{pathlib.Path(__file__).parent.resolve()}/data/discord-bot-process.json", "w")
+                    f.truncate(0)
+                    data = {"process-id": process.pid}
+                    json.dump(data, f)
+            except Exception as e:
+                flask.session["bot_action_response"] = str(e)
+        else:
+            flask.session["bot_action_response"] = "No discord bot start set."
         time.sleep(3)
         return flask.redirect("/bot")
 
 
 @dash.route("/iframe/administrator/log/login")
 async def render_iframe_login_log():
-    return flask.render_template("admin.html", iframe_src=f"http://{conf.HOST_IP}/administrator/log/login")
+    return flask.render_template("admin.html", iframe_src=f"http://{conf.HOST_IP}:{conf.PORT}/administrator/log/login")
 
 
 @dash.route("/iframe/administrator/log/notification")
 async def render_iframe_notif_log():
-    return flask.render_template("admin.html", iframe_src=f"http://{conf.HOST_IP}/administrator/log/notification")
+    return flask.render_template("admin.html", iframe_src=f"http://{conf.HOST_IP}:{conf.PORT}/administrator/log/notification")
 
 
 @dash.route("/iframe/server")
 async def render_iframe_server():
-    return flask.render_template("admin.html", iframe_src=f"http://{conf.HOST_IP}/server/stats/call")
+    return flask.render_template("admin.html", iframe_src=f"http://{conf.HOST_IP}:{conf.PORT}/server/stats/call")
 
 
 @dash.route("/setup", methods=["POST", "GET"])
@@ -306,69 +372,23 @@ async def setup():
 
 @dash.route("/setup/script/set", methods=["POST", "GET"])
 async def setup_set():
-    print(flask.request.form["command"])
-    if not flask.request.form.get("script-file") and not flask.request.form.get("start-command"):
-        flask.session["setup-msg"] = "You must set an option!"
-        return flask.redirect("/setup")
-    elif flask.request.form.get("script-file") and flask.request.form.get("start-command"):
-        flask.session["setup-msg"] = "You cannot set both options!"
-        return flask.redirect("/setup")
-    elif flask.request.form.get("script-file"):
-        if "setup-script-file" in flask.request.files:
-            file = flask.request.files["setup-script-file"]
-            if file.filename == "":
-                flask.session["setup-msg"] = "Your file needs to have a valid name!"
-                return flask.redirect("/setup")
-            elif file.filename.lower()[:-3].endswith(".py"):
-                try:
-                    f = open("./data/setup-start.json", "x")
-                    f_json = {
-                        "start-file-set": True,
-                        "start-file-path": None,
-                        "start-command-set": False,
-                        "start-command-command": None
-                    }
-                    json.dump(f_json, f)
-                except:
-                    traceback.print_exc()
-                    f = open("./data/setup-start.json", "w")
-                    f.truncate(0)
-                    f_json = {
-                        "start-file-set": True,
-                        "start-file-path": None,
-                        "start-command-set": False,
-                        "start-command-command": None
-                    }
-                    json.dump(f_json, f)
-
-
-        return flask.redirect("/setup")
-    elif flask.request.form.get("start-command"):
-        print(2)
-        command = flask.request.form["command"]
-        if command.lower().endswith(".py"):
-            print(1)
-            try:
-                f = open("./data/setup-start.json", "x")
-                f_json = {
-                    "start-file-set": False,
-                    "start-file-path": None,
-                    "start-command-set": True,
-                    "start-command-command": command
-                }
-                json.dump(f_json, f)
-            except:
-                traceback.print_exc()
-                f = open("./data/setup-start.json", "w")
-                f.truncate(0)
-                f_json = {
-                    "start-file-set": False,
-                    "start-file-path": None,
-                    "start-command-set": True,
-                    "start-command-command": command
-                }
-                json.dump(f_json, f)
-        return flask.redirect("/setup")
+    command = flask.request.form["command"]
+    try:
+        f = open(f"{pathlib.Path(__file__).parent.resolve()}/data/setup-start.json", "x")
+        f_json = {
+            "start-command-set": True,
+            "start-command-command": command
+        }
+        json.dump(f_json, f)
+    except FileExistsError:
+        f = open(f"{pathlib.Path(__file__).parent.resolve()}/data/setup-start.json", "w")
+        f.truncate(0)
+        f_json = {
+            "start-command-set": True,
+            "start-command-command": command
+        }
+        json.dump(f_json, f)
+    return flask.redirect("/")
 
 
 dash.run(host=conf.HOST_IP, port=conf.PORT, debug=conf.DEBUGGING)
